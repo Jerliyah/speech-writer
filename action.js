84c0f85ea95fd990fd56ff286755ddd31525d4af
Jerliyah <jerliyahcraig@gmail.com>
2018-02-02 13:58:31 -0600

    name change

--- action.js
+++ action.js
@@ -1,8 +1,19 @@
 /* ====== DOM Grab ====== */
-
+const ctn = document.querySelector('main');
 
 
 /* ====== Variables ====== */
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+// window.SpeechGrammarList = window.SpeechGrammarList || window.webkitSpeechGrammarList
+// window.SpeechRecognitionEvent = window.SpeechRecognitionEvent || window.webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
+recognition.interimResults = true;
+// recognition.start()
+
+// console.log(recognition)
+
 
 
 
